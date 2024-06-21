8b8501df128b03386af5e450c9b1c3bdb61ed9aa
クリシュナ クマール ケーシャブ <kumar-ke@MACN0066.local>
2024-06-21 16:49:02 +0900

    Sign Up Pageリファクタリング+設計修正

--- src/src/app/account/sign-up/page.tsx
+++ src/src/app/account/sign-up/page.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import SignUpForm from '@/features/routes/account/components/SignUp';
 import landingImage from '../../../images/landing.png';
+import landingFeatherUp from '../../../images/leafup.png';
 
 const SignUpPage = () => {
   return (
@@ -12,9 +13,9 @@ const SignUpPage = () => {
       </Head>
 
       {/* Background Image Container */}
-      <div className="relative min-h-screen">
+      <div className="relative min-h-screen bg-f8f9f7">
         {/* Background Image */}
-        <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-center bg-f8f9f7">
           <Image
             src={landingImage}
             alt="Landing Image"
@@ -26,10 +27,35 @@ const SignUpPage = () => {
         </div>
 
         {/* Content */}
-        <div className="absolute inset-0 flex items-center justify-center text-center text-gray-800">
-          <div className="bg-white p-8 rounded-lg shadow-lg max-w-md">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-gray-800">
+          <div>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">Wedding Gate</h1>
+            <div className="text-3xl md:text-4xl font-semibold mb-40">
+              <span className="text-green-600">大谷</span><br />
+              <span className="text-green-500">田中</span>
+            </div>
+            {/* Empty Character for Spacing */}
+            <div className="text-lg mb-80 text-custom-background font-semibold font-courier">
+            ۪
+            </div>
+          </div>
+        </div>
+        
+        {/* Sign-Up Form Section */}
+        <div className="absolute top-1/2 transform -translate-y-1/2 w-full text-center">
+          <div className="mt-8">
             <SignUpForm />
           </div>
+        </div>
+
+        {/* Leaf Image */}
+        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 mb-20">
+          <Image
+            src={landingFeatherUp}
+            alt="Leaf Image"
+            width={60}
+            height={100}
+          />
         </div>
       </div>
     </div>
